dba0de32b165c6a639301ccd64f84d07a7619d61
yuanxin <1017123610@qq.com>
2019-03-28 18:39:42 +0800

    test

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 function HeaderModle() {
     var mybody = document.getElementById('myBody');
     var header = document.createElement('div');
-    header.innerText = '我是哪个header啊';
+    header.innerText = '我是哪个header啊!';
     mybody.append(header);
 }
 
